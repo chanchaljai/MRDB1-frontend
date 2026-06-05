e34f420a9b99cf21c3ebb99e3b9560e7a503bea3
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-06-05 11:21:27 +0530

    bug fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mrdb1-backend.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
